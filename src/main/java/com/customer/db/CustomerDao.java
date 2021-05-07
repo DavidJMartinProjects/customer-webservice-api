bb2144aa9719ba6daaeb4caa8f606ecea3654374
unknown <davidjmartin@hotmail.com>
2021-05-07 23:39:08 +0100

    added logging to CustomerDao

--- src/main/java/com/customer/db/CustomerDao.java
+++ src/main/java/com/customer/db/CustomerDao.java
@@ -9,15 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.app.openapi.model.Customer;
-import com.customer.db.entity.CustomerEntity;
 import com.customer.db.repository.CustomerRepository;
 import com.customer.exceptions.ResourceNotFoundException;
 import com.customer.service.mapper.CustomerMapper;
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author davidjmartin
  */
+@Slf4j
 @Component
 public class CustomerDao {
 
@@ -28,31 +29,36 @@ public class CustomerDao {
     private CustomerRepository customerRepository;
 
     public List<Customer> findAllCustomers() {
+        log.info("fetching customers.");
         return customerRepository.findAll()
             .stream()
             .map(customerMapper::toDto)
             .collect(Collectors.toList());
     }
 
-    public Customer save(Customer customer) {
-        return customerMapper.toDto(
-            customerRepository.save(customerMapper.toEntity(customer))
-        );
-    }
-
     public Customer findCustomerById(@NonNull long id) {
+        log.info("fetching customer with id: {}.", id);
         return customerMapper.toDto(
             customerRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(format("resource with id: %s not found.", id)))
         );
     }
 
+    public Customer save(Customer customer) {
+        log.info("saving customer: {}.", customer);
+        return customerMapper.toDto(
+            customerRepository.save(customerMapper.toEntity(customer))
+        );
+    }
+
     public Customer updateCustomerById(Customer customer) {
+        log.info("updating customer with id: {} to {}.", customer.getId(), customer);
         return customerMapper.toDto(
             customerRepository.save(customerMapper.toEntity(customer))
         );
     }
 
     public void deleteCustomerById(@NonNull long id) {
+        log.info("deleting customer with id: {}.", id);
         customerRepository.deleteById(id);
     }
 
