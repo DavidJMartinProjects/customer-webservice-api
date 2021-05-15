3e9d9b6a54f717973266bb16e97cabecdc45e9bb
unknown <davidjmartin@hotmail.com>
2021-05-15 18:58:31 +0100

    refactoring CustomerControllerAdvice

--- src/main/java/com/customer/controller/CustomerController.java
+++ src/main/java/com/customer/controller/CustomerController.java
@@ -2,9 +2,11 @@ package com.customer.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +17,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.app.openapi.generated.api.CustomersApi;
-import com.app.openapi.generated.model.Customer;
 import com.customer.service.CustomerService;
-import lombok.extern.slf4j.Slf4j;
+
+import com.app.openapi.generated.model.Customer;
+import com.app.openapi.generated.api.CustomersApi;
+
 
 
 /**
