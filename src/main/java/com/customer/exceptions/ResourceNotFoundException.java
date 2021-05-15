a4fa5f608a341d0fe436c83df5bbae8c1a293f29
unknown <davidjmartin@hotmail.com>
2021-05-15 19:11:42 +0100

    refactoring CustomerControllerAdvice

--- src/main/java/com/customer/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/customer/exceptions/ResourceNotFoundException.java
@@ -1,16 +1,10 @@
 package com.customer.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 /**
  * @author davidjmartin
  */
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-
     public ResourceNotFoundException(String message) {
         super(message);
     }
-
 }
