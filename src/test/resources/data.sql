c4a2c67e11d854ca312a2ba9cc22a887b8826a6b
unknown <davidjmartin@hotmail.com>
2021-06-12 18:44:06 +0100

    updated data.sql

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -14,12 +14,12 @@ CREATE TABLE customers (
 INSERT INTO customers (first_name, last_name, address, city, country, email, image) VALUES
   ('David', 'Martin', 'Main St.', 'Texas', 'USA', 'dm@email.com', 'https://bootdey.com/img/Content/avatar/avatar1.png'),
   ('Vicky', 'Hulston', 'City Center', 'Paris', 'France', 'jd@email.com', 'https://bootdey.com/img/Content/avatar/avatar2.png'),
-  ('Mary', 'Bloggs', 'Town Square', 'London', 'UK', 'mb@email.com', 'https://bootdey.com/img/Content/avatar/avatar.png'),
+  ('Mary', 'Bloggs', 'Town Square', 'London', 'UK', 'mb@email.com', 'https://bootdey.com/img/Content/avatar/avatar5.png'),
   ('Jim', 'Martin', 'Main St.', 'Texas', 'USA', 'dm@email.com', 'https://bootdey.com/img/Content/avatar/avatar4.png'),
   ('Joe', 'Doe', 'City Center', 'Paris', 'France', 'jd@email.com', 'https://bootdey.com/img/Content/avatar/avatar5.png'),
   ('Lucy', 'Bloggs', 'Town Square', 'London', 'UK', 'mb@email.com', 'https://bootdey.com/img/Content/avatar/avatar6.png'),
   ('Ann', 'Martin', 'Main St.', 'Texas', 'USA', 'dm@email.com', 'https://bootdey.com/img/Content/avatar/avatar7.png'),
   ('John', 'Doe', 'City Center', 'Paris', 'France', 'jd@email.com', 'https://bootdey.com/img/Content/avatar/avatar8.png'),
-  ('Mary', 'Bloggs', 'Town Square', 'London', 'UK', 'mb@email.com', 'https://bootdey.com/img/Content/avatar/avatar9.png'),
+  ('Mary', 'Bloggs', 'Town Square', 'London', 'UK', 'mb@email.com', 'https://bootdey.com/img/Content/avatar/avatar2.png'),
   ('Jane', 'Martin', 'Main St.', 'Texas', 'USA', 'dm@email.com', 'https://bootdey.com/img/Content/avatar/avatar1.png'),
   ('John', 'Doe', 'City Center', 'Paris', 'France', 'jd@email.com', 'https://bootdey.com/img/Content/avatar/avatar2.png');
